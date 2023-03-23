313766cb391fa35963bf7d0b74b6211c22535d3f
RaylILYn <rednoxmc@mail.ru>
2023-03-23 18:09:09 +0300

    Добавьте файлы проекта.

---
+++ ThreeMathFunction/ThreeMathFunction/ThreeMathFunction.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float x, y;
+	cout << "Введите значение X: ";
+	cin >> x;
+	if (x == -10)
+	{
+		y = 1 / x;
+		cout << "Формула 3" << endl;
+	}
+	else {
+		if (x <= 0)
+		{
+			y = pow(x, 2);
+			cout << "Формула 1 " << endl;
+		}
+		else {
+			if (x > 1)
+			{
+				y = sqrt(x + 1);
+				cout << "Формула 2" << endl;
+			}
+			else {
+				y = 1 / x;
+				cout << "Формула 3" << endl;
+			}
+		}
+	}
+	cout.precision(5);
+	cout << "Функция Y равна: " << y << endl << endl;
+	system("pause");
+}
